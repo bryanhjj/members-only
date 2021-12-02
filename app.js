ea3e9c509a20d6cd3cb08dc1ebd4f41480f21604
Bryan <bryan.how@live.com>
2021-12-02 19:24:47 +0800

    Set config for heroku deployment

--- app.js
+++ app.js
@@ -92,4 +92,4 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log("app listening on port 3000!"));
